642de95d83c256086fa237c49be3491dbacc24c5
aposalo <apostolissalomidis@yahoo.gr>
2022-11-27 22:33:17 +0200

    Finished flow

--- app/src/main/java/android/example/com/currencycalculator/model/CurrencyCalculatorModel.kt
+++ app/src/main/java/android/example/com/currencycalculator/model/CurrencyCalculatorModel.kt
@@ -3,45 +3,31 @@ package android.example.com.currencycalculator.model
 import android.example.com.currencycalculator.repository.CurrencyCalculatorRepository
 import android.example.com.currencycalculator.repository.dto.FixerDto
 import android.example.com.currencycalculator.util.Resource
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import retrofit2.Response
 
-class CurrencyCalculatorModel(
-    private val repository: CurrencyCalculatorRepository,
-    private var previousAmount: Float ,
-    private var previousTo: String,
-    private var previousFrom: String,
-) : ViewModel() {
+class CurrencyCalculatorModel() : ViewModel() {
 
-    val data: MutableLiveData<Resource<FixerDto>> = MutableLiveData()
-
+    private val _dataFlow = MutableStateFlow<Resource<FixerDto>>(Resource.Success(null))
+    private val repository: CurrencyCalculatorRepository = CurrencyCalculatorRepository()
     private var finalResponse: Response<FixerDto>? = null
 
-    fun getUserPage(to: String, from: String, amount: Float) =  viewModelScope.launch {
-        previousTo = to
-        previousFrom = from
-        previousAmount = amount
-        if (amount <= 0.0f) {
-            finalResponse = null
-            data.postValue(handlePageResponse())
-        }
-        else {
-            data.postValue(Resource.Loading())
-            val response = repository.getFixerConvert(to, from, amount)
-            if (response.isCurrentResponse()){
-                finalResponse = response
-                data.postValue(handlePageResponse())
-            }
-        }
-    }
+    val data = _dataFlow.asStateFlow()
 
-    private fun Response<FixerDto>?.isCurrentResponse(): Boolean {
-        return this?.body()?.query?.amount == previousAmount
-                && this.body()?.query?.to == previousTo
-                && this.body()?.query?.from == previousFrom
+    fun getUserPage(to: String, from: String, amount: Float) {
+        viewModelScope.launch {
+            finalResponse = null
+            if (amount > 0.0f) {
+                _dataFlow.value = Resource.Loading()
+                val response = repository.getFixerConvert(to, from, amount)
+                finalResponse = response
+            }
+            _dataFlow.value = handlePageResponse()
+        }
     }
 
     private fun handlePageResponse() : Resource<FixerDto> {
