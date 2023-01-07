eca094963e3b128c3277c4b8bd9b46faf244d46e
aposalo <apostolissalomidis@yahoo.gr>
2023-01-07 15:33:17 +0200

    Moved listener to another package

--- app/src/main/java/aposalo/com/currencycalculator/MainActivity.kt
+++ app/src/main/java/aposalo/com/currencycalculator/MainActivity.kt
@@ -1,8 +1,5 @@
 package aposalo.com.currencycalculator
 
-import aposalo.com.currencycalculator.databinding.ActivityMainBinding
-import aposalo.com.currencycalculator.model.CurrencyCalculatorModel
-import aposalo.com.currencycalculator.util.Extensions.Companion.getCalculation
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -11,11 +8,13 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.button.MaterialButton
+import aposalo.com.currencycalculator.databinding.ActivityMainBinding
+import aposalo.com.currencycalculator.listeners.CalculatorListener
+import aposalo.com.currencycalculator.model.CurrencyCalculatorModel
 
 const val TAG = "MainActivity"
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var currencyArray: Array<String>
 
@@ -48,26 +47,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding.resultSpinner.setSpinner(1)
         binding.currencySpinner.setSpinner(0)
 
-        binding.x.setOnClickListener(this)
-        binding.openBracket.setOnClickListener(this)
-        binding.closeBracket.setOnClickListener(this)
-        binding.divide.setOnClickListener(this)
-        binding.multiply.setOnClickListener(this)
-        binding.plus.setOnClickListener(this)
-        binding.minus.setOnClickListener(this)
-        binding.equals.setOnClickListener(this)
-        binding.zero.setOnClickListener(this)
-        binding.one.setOnClickListener(this)
-        binding.two.setOnClickListener(this)
-        binding.three.setOnClickListener(this)
-        binding.four.setOnClickListener(this)
-        binding.five.setOnClickListener(this)
-        binding.six.setOnClickListener(this)
-        binding.seven.setOnClickListener(this)
-        binding.eight.setOnClickListener(this)
-        binding.nine.setOnClickListener(this)
-        binding.c.setOnClickListener(this)
-        binding.dot.setOnClickListener(this)
+        val listener = CalculatorListener(binding, resources)
+
+        binding.x.setOnClickListener(listener)
+        binding.openBracket.setOnClickListener(listener)
+        binding.closeBracket.setOnClickListener(listener)
+        binding.divide.setOnClickListener(listener)
+        binding.multiply.setOnClickListener(listener)
+        binding.plus.setOnClickListener(listener)
+        binding.minus.setOnClickListener(listener)
+        binding.equals.setOnClickListener(listener)
+        binding.zero.setOnClickListener(listener)
+        binding.one.setOnClickListener(listener)
+        binding.two.setOnClickListener(listener)
+        binding.three.setOnClickListener(listener)
+        binding.four.setOnClickListener(listener)
+        binding.five.setOnClickListener(listener)
+        binding.six.setOnClickListener(listener)
+        binding.seven.setOnClickListener(listener)
+        binding.eight.setOnClickListener(listener)
+        binding.nine.setOnClickListener(listener)
+        binding.c.setOnClickListener(listener)
+        binding.dot.setOnClickListener(listener)
     }
 
 
@@ -95,47 +96,4 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         viewModel.getUserPage(toSelectedItem, fromSelectedItem, result.toFloat())
     }
 
-    private fun clearCalculator() {
-        binding.solutionTv.text = ""
-        binding.resultTv.text = resources.getString(R.string.init_value)
-    }
-
-    override fun onClick(view: View) {
-        val button = view as MaterialButton
-        val buttonText = button.text.toString()
-        var dataToCalculate = binding.solutionTv.text.toString()
-        val result = binding.resultTv.text.toString();
-        when (buttonText) {
-            resources.getString(R.string.clear_button) -> {
-                clearCalculator()
-                return
-            }
-            resources.getString(R.string.equal_button) -> {
-                binding.solutionTv.text = result
-                return
-            }
-            resources.getString(R.string.delete_button) -> {
-                if (!binding.solutionTv.text.isNullOrEmpty()) {
-                    dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length - 1)
-                    if (dataToCalculate.isEmpty()) {
-                        clearCalculator()
-                        return
-                    }
-                }
-                else {
-                    clearCalculator()
-                    return
-                }
-            }
-            else -> {
-                dataToCalculate += buttonText
-            }
-        }
-        binding.solutionTv.text = dataToCalculate
-        val finalResult = dataToCalculate.getCalculation()
-
-        if (finalResult != "Err" && finalResult != result) {
-            binding.resultTv.text = finalResult
-        }
-    }
 }
